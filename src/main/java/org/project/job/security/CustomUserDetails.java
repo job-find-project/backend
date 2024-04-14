803d5be0896c0a789873bb9c3572d9a02100b95d
Tran Hoang Quan <quan02042004@gmail.com>
2024-04-14 18:27:07 +0700

    finish login, register

---
+++ src/main/java/org/project/job/security/CustomUserDetails.java
@@ -0,0 +1,69 @@
+package org.project.job.security;
+
+import lombok.Data;
+import org.project.job.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class CustomUserDetails implements UserDetails {
+    private String userName;
+    private String password;
+    private Boolean isEnabled;
+    private List<GrantedAuthority> authorities;
+
+    public CustomUserDetails(User user) {
+        this.userName = user.getEmail();
+        this.password = user.getPassword();
+        this.isEnabled = user.getIsEnabled();
+//        this.authorities =
+//                Arrays.stream(user.getRoles().toString().split(","))
+//                        .map(SimpleGrantedAuthority::new)
+//                        .collect(Collectors.toList());
+        this.authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+        System.out.println(user+ " " + authorities);
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
