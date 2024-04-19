ff203063aa29c3ddd120de42bb2d529ea04d7b84
Tran Hoang Quan <quan02042004@gmail.com>
2024-04-19 15:30:31 +0700

    add origin

--- src/main/java/org/project/job/controller/UserController.java
+++ src/main/java/org/project/job/controller/UserController.java
@@ -3,15 +3,13 @@ package org.project.job.controller;
 import org.project.job.entity.User;
 import org.project.job.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController {
 
     @Autowired private UserService userService;
