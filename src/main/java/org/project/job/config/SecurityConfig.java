4c5c44f1ef0fd5ee45ab33549d80ea17a45c6525
Tran Hoang Quan <quan02042004@gmail.com>
2024-04-19 16:24:33 +0700

    fix cor2

--- src/main/java/org/project/job/config/SecurityConfig.java
+++ src/main/java/org/project/job/config/SecurityConfig.java
@@ -21,6 +21,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -29,6 +30,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@CrossOrigin(origins = "*")
 public class SecurityConfig {
 
     @Autowired private UserDetailsService userDetailsService;
